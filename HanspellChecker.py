dcbc3b1ead55c723073be798f24a5325ca6dee6c
Radia <songblue61@naver.com>
2020-04-12 15:08:20 +0900

    Update HanspellChecker.py
    - emoji 제거 메서드 추가
    - 교정 후 파일로 저장하는 메서드 추가

--- HanspellChecker.py
+++ HanspellChecker.py
@@ -1,4 +1,5 @@
 import requests
+from pathlib import Path
 import json
 import re
 import time
@@ -8,38 +9,59 @@ from requests.exceptions import HTTPError
 class HanspellChecker:
 
     def __init__(self):
-         # we'll use Naver spell checker
-        self.base_url = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy'
+        # we'll use Naver spell checker
+        self.base_url = (
+            "https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy"
+        )
         self._agent = requests.Session()
 
-
     def remove_tag(self, text):
-        text = re.sub('<.+?>', '', text, 0, re.I|re.S)
+        text = re.sub("<.+?>", "", text, 0, re.I | re.S)
         return text
 
+    def remove_emoji(self, text):
+        emoji = re.compile("["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            u"\U00002500-\U00002BEF"  # chinese char
+            u"\U00002702-\U000027B0"
+            u"\U00002702-\U000027B0"
+            u"\U0001f926-\U0001f937"
+            u"\U00010000-\U0010ffff"
+            u"\u2640-\u2642" 
+            u"\u2600-\u2B55"
+            u"\u200d"
+            u"\u23cf"
+            u"\u23e9"
+            u"\u231a"
+            u"\ufe0f"  # dingbats
+            u"\u3030"
+                        "]+", re.UNICODE)
+        return re.sub(emoji, '', text)  
 
     def correct(self, text):
         """
         This function checks the spelling and grammer of korean text,
         and corrects it.
         """
-
         if isinstance(text, list):
             result = []
-            for item in text:
+            for idx, item in enumerate(text):
                 corrected = self.correct(item)
                 result.append(corrected)
+                print("[-] {}/{} complete".format(idx+1, len(text)))
             return result
 
         # Max length of text is 500
         if len(text) > 500:
             return text
-        
-        params = {
-            'q': text,
-            'color_blindness': 0
-        }
 
+        # Remove emoji
+        text = self.remove_emoji(text)
+
+        params = {"q": text, "color_blindness": 0}
         # Get response
         success = False
         try_cnt = 0
@@ -53,23 +75,44 @@ class HanspellChecker:
                         return text
                     raise HTTPError
             except HTTPError:
-                print("[!] An error occurred while sending the request. error code: {}".format(res_code))
+                print(
+                    "[!] An error occurred while sending the request. error code: {}".format(
+                        res_code
+                    )
+                )
                 print("[-] Waiting 5 seconds before resending the request...")
                 time.sleep(5)
             else:
                 success = True
-                
+
         r = r.text  # change bytestring to string
         data = json.loads(r)
-        html = data['message']['result']['html']
+        html = data["message"]["result"]["html"]
 
         # Remove html tag in the response
         result = self.remove_tag(html)
-        
+
         return result
 
+    def correct_and_save(self, filename):
+        """
+        correct sentences line by line in the file, and save it in a new file(.txt).
+        """
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                text_list = f.read().split('\n')
 
-if __name__ == '__main__':
-    
+        except FileNotFoundError as e:
+            print(e)
+            return -1
+        
+        corrected = self.correct(text_list)
+
+        new_filename = Path(filename).resolve().stem + "_corrected.txt"
+        with open(new_filename, "w", encoding="utf-8") as f:
+            f.write("\n".join(corrected))
+
+
+if __name__ == "__main__":
     checker = HanspellChecker()
-    print(checker.correct("안녕하세여. 자연어처리는 넘 어려워용."))
+    print(checker.correct("안녕하세영. 자연어처리는 너무어려워용"))
